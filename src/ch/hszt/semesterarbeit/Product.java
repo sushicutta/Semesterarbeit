9aad026477ef3915f8ec76b197f85cd1ea815d46
Roman Wuersch <sushicutta@gmail.com>
2011-02-02 23:21:07 +0100

    SerialID angepasst von ch.hszt.semesterarbeit.Product

--- src/ch/hszt/semesterarbeit/Product.java
+++ src/ch/hszt/semesterarbeit/Product.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 
 public class Product implements Serializable, Comparable<Product> {
 	
-	private static final long serialVersionUID = 666L;
+	private static final long serialVersionUID = 4230587722117557975L;
 
 	public static String OBJECT_NAME = "product";
 	
